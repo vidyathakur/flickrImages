ac1e1ceec5a565eb36e3e5b1fc0f5d5f4bf60db3
Vidya Thakur <vidya.thakur@medimojo.in>
2020-07-23 12:57:00 +0530

    flickrImages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImagedetailsComponent } from './imagedetails/imagedetails.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'imagedetails/:id', component:ImagedetailsComponent},
+  {path:'gallery', component:GalleryComponent},
+  {
+		path: '**',
+		redirectTo: 'gallery'
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ImagedetailsComponent } from './imagedetails/imagedetails.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagedetailsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InfiniteScrollModule,
+    NgxSpinnerModule,
     AppRoutingModule
   ],
   providers: [],
